843edcae4a7e961a0f34fb5e095efdb3b273ec88
Maicol <maicolcubidespe@gmail.com>
2023-11-12 21:08:49 -0500

    Subiendo mini proyecto react con vite.js

---
+++ src/data/tasks.js
@@ -0,0 +1,24 @@
+export const tasks = [
+    {
+        id: 1,
+        title: 'mi primer tarea',
+        description: 'Mi primera chamba'
+    },
+    {
+        id: 2,
+        title: 'mi segunda tarea',
+        description: 'Mi segunda chamba'
+    },
+    {
+        id: 3,
+        title: 'mi tercer tarea',
+        description: 'Mi tercera chamba'
+    },
+    {
+        id: 4,
+        title: 'mi cuarta tarea',
+        description: 'Mi cuarta chamba'
+    },
+];
+
+
